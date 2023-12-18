3ecd02278c6acacfb5b5f3d75eb4005cd1aafac7
Léo Hervouet <leoohervouet@gmail.com>
2023-12-18 10:05:40 +0100

    Add GraphUsingAdjacencyMatrix

---
+++ src/graphes/GraphUsingAdjacectyMatrix.java
@@ -0,0 +1,57 @@
+package graphes;
+
+import java.util.Iterator;
+
+public class GraphUsingAdjacectyMatrix extends Graph
+{
+    protected int[] pro_adjacencyMatrix;
+
+    public GraphUsingAdjacectyMatrix(int vertices)
+    {
+        super(vertices);
+        pro_adjacencyMatrix = new int[pro_vertices * pro_vertices];
+    }
+
+    @Override
+    public void addEdge(int source, int destination, int weight)
+    {
+        pro_adjacencyMatrix[source * pro_vertices + destination] = weight;
+    }
+
+    @Override
+    public int getWeight(int source, int destination)
+    {
+        return pro_adjacencyMatrix[source * pro_vertices + destination];
+    }
+
+    @Override
+    public Iterator<Integer> getNeighbors(int vertex)
+    {
+        return new MatrixIterator(vertex);
+    }
+
+    // Iterator to iterate through all neighbors the vertices of a vertex.
+    private class MatrixIterator implements Iterator<Integer>
+    {
+        private final int pri_vertex;
+        private int pri_currentNeighbor;
+
+        public MatrixIterator(int vertex)
+        {
+            this.pri_vertex = vertex;
+            this.pri_currentNeighbor = 0;
+        }
+
+        @Override
+        public boolean hasNext()
+        {
+            return pri_currentNeighbor < pro_vertices;
+        }
+
+        @Override
+        public Integer next()
+        {
+            return pro_adjacencyMatrix[pri_vertex * pro_vertices + pri_currentNeighbor++];
+        }
+    }
+}
